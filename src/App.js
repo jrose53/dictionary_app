2858b912c6a09f8ffcca18761fd7b3d4c45805a9
jrose53 <107949691+jrose53@users.noreply.github.com>
2024-04-24 16:14:27 +0200

    Creating the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from "./logo.gif";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          {" "}
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          {" "}
+          <small> Coded by Juliette Rose </small>
+        </footer>
+      </div>
     </div>
   );
 }
